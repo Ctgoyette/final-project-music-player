b5953b232e919c9cc9d99e94af3a3ea205f92890
Ctgoyette <ctgoyette21@gmail.com>
2022-12-02 13:39:14 -0500

    Added playlist content page

--- gui.py
+++ gui.py
@@ -8,17 +8,18 @@ class PlayerWindow(Ui_MainWindow):
     def __init__(self):
         self.MainWindow = QtWidgets.QMainWindow()
         self.setupUi(self.MainWindow)
-        self.artist_dropdown_index = self.library_list_view_dropdown.findText('Artists')
-        self.album_dropdown_index = self.library_list_view_dropdown.findText('Albums')
-        self.song_dropdown_index = self.library_list_view_dropdown.findText('Tracks')
-        self.library_list_view_dropdown.currentIndexChanged.connect(self.switch_list_view)
-        self.artist_list_index = 0
-        self.album_list_index = 1
-        self.song_list_index = 1
-        self.switch_list_view()
+        self.library_dropdown_artist_index = self.library_view_dropdown.findText('Artists')
+        self.library_dropdown_album_index = self.library_view_dropdown.findText('Albums')
+        self.library_dropdown_song_index = self.library_view_dropdown.findText('Tracks')
+        self.library_view_dropdown.currentIndexChanged.connect(self.switch_library_view)
         self.song_list.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         self.artist_list.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         self.album_list.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.switch_library_view()
+        self.button_library_view.clicked.connect(lambda: self.switch_content_view('Library'))
+        self.button_playlist_view.clicked.connect(lambda: self.switch_content_view('Playlists'))
+        self.playlist_list.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.switch_content_view('Library')
 
     def add_table_row(self, table):
         row_count = table.rowCount()
@@ -36,16 +37,25 @@ class PlayerWindow(Ui_MainWindow):
             row += 1
         table.resizeColumnsToContents()
 
-    def switch_list_view(self):
-        index = self.library_list_view_dropdown.currentIndex()
-        if  index == self.artist_dropdown_index:
+    def switch_library_view(self):
+        index = self.library_view_dropdown.currentIndex()
+        if index == self.library_dropdown_artist_index:
             self.library_view_stack.setCurrentIndex(0)
-        elif index == self.album_dropdown_index:
+        elif index == self.library_dropdown_album_index:
             self.library_view_stack.setCurrentIndex(1)
-        elif index == self.song_dropdown_index:
+        elif index == self.library_dropdown_song_index:
             self.library_view_stack.setCurrentIndex(2)
         else:
             pass
+    
+    def switch_content_view(self, selected_content_page):
+        if selected_content_page == 'Library':
+            self.content_type_view_stack.setCurrentIndex(0)
+        elif selected_content_page == 'Playlists':
+            self.content_type_view_stack.setCurrentIndex(1)
+        else:
+            pass
+
 
 
 
